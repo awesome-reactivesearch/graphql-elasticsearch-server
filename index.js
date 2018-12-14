f990ed1380a0eb2cdf9c26e105155733c4288108
lakhansamani <lakhan.m.samani@gmail.com>
2018-12-14 08:52:58 +0530

    Update use of cors

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ const generatedSchema = new GraphQLSchema({
 });
 
 const server = express();
-server.use(cors());
 
 server.use(
   '/',
+  cors(),
   graphqlHTTP({
     schema: generatedSchema,
     graphiql: true,
